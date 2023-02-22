5b20fdd4f0dcdd1c1eb4bc5d7219f5065c5a77f7
luo jun <luojun96@live.cn>
2023-02-22 17:22:29 +0800

    1. fix issue in Dockerfile; 2. deploy admission webhook server.

--- resources/scripts/deploy.sh
+++ resources/scripts/deploy.sh
@@ -9,4 +9,23 @@ keydir="$(mktemp -d)"
 echo "Generating TLS keys ..."
 "${basedir}/ssl/genssl.sh" "$keydir"
 
+echo "Creating Kubernetes objects..."
+# Create the `admission-webhook` namespace. This cannot be part of the YAML file as we first need to create The TLS secret.
 
+kubectl create namespace admission-webhook
+
+# Create the TLS secret for the generated keys.
+kubectl -n admission-webhook create secret tls admission-webhook-server-tls \
+    --cert "${keydir}/k8s-admission-webhook-server.crt" \
+    --key "${keydir}/k8s-admission-webhook-server.key"
+
+# Read the PEM-encoded CA certificate, base64 encode it, and replace the `${CA_PEM_B64}` placeholder in the YAML template with it.
+# Then, create the Kubernetes resources.
+ca_pem_b64="$(openssl base64 -A <"${keydir}/ca.crt")"
+sed -e 's/${CA_PEM_B64}/'"$ca_pem_b64"'/g' <"${basedir}/deployment.yaml.template" \
+    | kubectl create -f -
+
+# Delete the key directory
+rm -rf "$keydir"
+
+echo "The admission webhook server has been deployed and configured."
--- resources/scripts/ssl/genssl.sh
+++ resources/scripts/ssl/genssl.sh
@@ -14,14 +14,14 @@ req_extensions = v3_req
 distinguished_name = req_distinguished_name
 prompt = no
 [req_distinguished_name]
-CN = luojun96.k8s-admission-webhook-server.svc
+CN = admission-webhook-server.admission-webhook.svc
 [ v3_req ]
 basicConstraints = CA:FALSE
 keyUsage = nonRepudiation, digitalSignature, keyEncipherment
 extendedKeyUsage = clientAuth, serverAuth
 subjectAltName = @alt_names
 [alt_names]
-DNS.1 = luojun96.k8s-admission-webhook-server.svc
+DNS.1 = admission-webhook-server.admission-webhook.svc
 EOF
 
 # Generate the CA cert and private key
@@ -29,5 +29,5 @@ openssl req -nodes -new -x509 -keyout ca.key -out ca.crt -subj "/CN=Admission Co
 # Generate the private key for the webhook server
 openssl genrsa -out k8s-admission-webhook-server.key 2048
 # Generate a Certificate Signing Request (CSR) for the private key, and sign it with private key of the CA.
-openssl req -new -key k8s-admission-webhook-server.key -subj "/CN=luojun96.k8s-admission-webhook-server.svc" -config server.conf \
+openssl req -new -key k8s-admission-webhook-server.key -subj "/CN=admission-webhook-server.admission-webhook.svc" -config server.conf \
     | openssl x509 -req -CA ca.crt -CAkey ca.key -CAcreateserial -out k8s-admission-webhook-server.crt -extensions v3_req -extfile server.conf
