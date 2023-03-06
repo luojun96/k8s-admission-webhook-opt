a2e4326261f138fb50aa92c7776968b285968e18
luo jun <luojun96@live.cn>
2023-03-06 21:50:28 +0800

    separate liveness and readness check api.

--- cmd/webhook-server/health.go
+++ cmd/webhook-server/health.go
@@ -9,31 +9,32 @@ import (
 	"time"
 )
 
+type doHealthCheck func() error
+
 func newHealthCheckServer() *http.Server {
 	mux := http.NewServeMux()
-	mux.Handle("/healthz", healthCheckHandler())
+	mux.Handle("/ready", healthCheckHandler(doDNSResolveCheck))
+	mux.Handle("/live", healthCheckHandler(doLivenessCheck))
 	return &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 }
 
-func healthCheckHandler() http.Handler {
+func healthCheckHandler(check doHealthCheck) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		serveHealthCheckFunc(w, r)
+		serveHealthCheckFunc(w, r, check)
 	})
 }
 
-func serveHealthCheckFunc(w http.ResponseWriter, r *http.Request) {
-	log.Print("Handling readiness check request ... ")
+func serveHealthCheckFunc(w http.ResponseWriter, r *http.Request, check doHealthCheck) {
 	var writeErr error
-	err := doDNSResolveCheck("kubernetes.default.svc", 50*time.Millisecond)
+	err := check()
 	if err != nil {
-		log.Printf("Error handling readiness check request: %v", err)
+		log.Printf("Error handling health check request: %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		_, writeErr = w.Write([]byte(err.Error()))
 	} else {
-		log.Printf("Readiness check request handled successfully")
 		_, writeErr = w.Write([]byte("ok"))
 	}
 
@@ -42,13 +43,15 @@ func serveHealthCheckFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func doDummyCheck() error {
+func doLivenessCheck() error {
 	return nil
 }
 
 // DNSResolveCheck returns a Check that makes sure the provided host can resolve
 // to at least one IP address within the specified timeout.
-func doDNSResolveCheck(host string, timeout time.Duration) error {
+func doDNSResolveCheck() error {
+	host := "kubernetes.default.svc"
+	timeout := 50 * time.Millisecond
 	resolver := net.Resolver{}
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
