d2e0e8640f32582a085ce942d71aa9a7558c357f
Jun Luo <luojun96@live.cn>
2023-03-30 16:18:40 +0800

    add test templates

---
+++ cmd/webhook-server/health_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestHealth(t *testing.T) {
+	// ...
+}
+
+func TestHealthHandler(t *testing.T) {
+	// ...
+}
+
+func TestNewHealthHandler(t *testing.T) {
+	// ...
+}
+
+func TestHealthHandlerServeHTTP(t *testing.T) {
+	// ...
+}
